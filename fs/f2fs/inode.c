3884cc6b269829a98381c1667380a91e55998457
Yeongjin Gil <youngjin.gil@samsung.com>
2025-07-29 11:02:37 +0500

    BACKPORT: f2fs: Prevent s_writer rw_sem count mismatch in f2fs_evict_inode If f2fs_evict_inode is called between freeze_super and thaw_super, the s_writer rwsem count may become negative, resulting in hang.
    CPU1                       CPU2
    
    f2fs_resize_fs()           f2fs_evict_inode()
      f2fs_freeze
        set SBI_IS_FREEZING
                                 skip sb_start_intwrite
      f2fs_unfreeze
        clear SBI_IS_FREEZING
                                 sb_end_intwrite
    
    To solve this problem, the call to sb_end_write is determined by whether
    sb_start_intwrite is called, rather than the current freezing status.
    
    Reviewed-by: Sungjong Seo <sj1557.seo@samsung.com>
    Reviewed-by: Sunmin Jeong <s_min.jeong@samsung.com>
    Signed-off-by: Yeongjin Gil <youngjin.gil@samsung.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -770,6 +770,7 @@ void f2fs_evict_inode(struct inode *inode)
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	nid_t xnid = F2FS_I(inode)->i_xattr_nid;
 	int err = 0;
+	bool freeze_protected = false;
 
 	f2fs_abort_atomic_write(inode, true);
 
@@ -803,8 +804,10 @@ void f2fs_evict_inode(struct inode *inode)
 	f2fs_remove_ino_entry(sbi, inode->i_ino, UPDATE_INO);
 	f2fs_remove_ino_entry(sbi, inode->i_ino, FLUSH_INO);
 
-	if (!is_sbi_flag_set(sbi, SBI_IS_FREEZING))
+	if (!is_sbi_flag_set(sbi, SBI_IS_FREEZING)) {
 		sb_start_intwrite(inode->i_sb);
+		freeze_protected = true;
+	}
 	set_inode_flag(inode, FI_NO_ALLOC);
 	i_size_write(inode, 0);
 retry:
@@ -861,7 +864,7 @@ retry:
 			set_sbi_flag(sbi, SBI_NEED_FSCK);
 		}
 	}
-	if (!is_sbi_flag_set(sbi, SBI_IS_FREEZING))
+	if (freeze_protected)
 		sb_end_intwrite(inode->i_sb);
 no_delete:
 	dquot_drop(inode);
