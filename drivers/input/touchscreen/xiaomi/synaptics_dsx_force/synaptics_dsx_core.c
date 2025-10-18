94239e5af35fdaf49b7524bc7a3b9a81609d18e8
Diogo Ferreira <defer@cyngn.com>
2025-10-19 02:16:07 +0500

    input: touchscreen: synaptics_dsx_force: Properly setup soft button irqs on resume
    If soft-buttons are disabled, their interrupt should also be disabled
    upon initialization. This patch reconfigures the soft-button function
    irqs upon suspend/resume so that the disabled state survives after
    coming out of suspension.
    
    Change-Id: Iec8242608754af0761ebb9f1e5ff12c2f8c7415a

--- drivers/input/touchscreen/xiaomi/synaptics_dsx_force/synaptics_dsx_core.c
+++ drivers/input/touchscreen/xiaomi/synaptics_dsx_force/synaptics_dsx_core.c
@@ -200,6 +200,8 @@ static void drm_regulator_ctrl(struct synaptics_rmi4_data *rmi4_data, unsigned i
 static void drm_reset_ctrl(const struct synaptics_dsx_board_data *bdata, bool on);
 static void drm_reset_action(const struct synaptics_dsx_board_data *bdata);
 
+static void synaptics_key_ctrl(struct synaptics_rmi4_data *rmi4_data, bool enable);
+
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #ifndef CONFIG_DRM
 #define USE_EARLYSUSPEND
@@ -2074,6 +2076,8 @@ static int synaptics_rmi4_int_enable(struct synaptics_rmi4_data *rmi4_data,
 			}
 		}
 	}
+
+	synaptics_key_ctrl(rmi4_data, enable && rmi4_data->button_0d_enabled);
 
 	return retval;
 }
