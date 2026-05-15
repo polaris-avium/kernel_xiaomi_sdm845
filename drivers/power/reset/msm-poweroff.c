d09f042da62567ca59304e4d3b9f3a9a2c1ae579
jsdizkcksv <3531236823@qq.com>
2026-05-15 20:38:05 +0800

    debug

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -498,16 +498,19 @@ static void msm_restart_prepare(const char *cmd)
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 
-        // Force Reboot Recovery
-        /*
-        patch
-        */
-        
 	qpnp_pon_set_restart_reason(
-	    PON_RESTART_REASON_RECOVERY);
-	__raw_writel(0x77665502, restart_reason);        
+            PON_RESTART_REASON_RECOVERY);
+         __raw_writel(0x77665502, restart_reason);
         flush_cache_all();
         return;
+
+	if (in_panic) {
+		// Reboot to recovery
+		qpnp_pon_set_restart_reason(
+			PON_RESTART_REASON_RECOVERY);
+		__raw_writel(0x77665502, restart_reason);
+		goto finish_set_restart_reason;
+	}
 
 	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
